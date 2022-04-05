3da6f048a9d3ffe7232797b0935f7d6c078190e6
Victor Gasior <vicagasi@ut.utm.edu>
2022-04-05 09:16:37 -0500

    - Set up basic routing

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import firebase from "firebase";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyClqrbbfZZEWZs8bvPs2b4aJSzxbjFVjxQ",
-    authDomain: "vueconnect4.firebaseapp.com",
-    projectId: "vueconnect4",
-    storageBucket: "vueconnect4.appspot.com",
-    messagingSenderId: "1068821387814",
-    appId: "1:1068821387814:web:ce2ea81f6cfffcd444e4e7"
-  };
-  
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = createApp(App);
 
-Vue.config.productionTip = false;
+app.use(router);
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: () => import("../views/Home.vue"),
+    },
+    {
+      path: "/register",
+      component: () => import("../views/Register.vue"),
+    },
+    {
+      path: "/sign-in",
+      component: () => import("../views/SignIn.vue"),
+    },
+    {
+      path: "/feed",
+      component: () => import("../views/Feed.vue"),
+    },
+  ],
 });
 
 export default router;
+
