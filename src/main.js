97c450d36cc94c4625e3636ac87214ba6afe570a
Victor Gasior <vicagasi@ut.utm.edu>
2022-04-05 09:07:42 -0500

    - the beginning

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import firebase from "firebase";
 
-createApp(App).use(router).mount("#app");
+const firebaseConfig = {
+    apiKey: "AIzaSyClqrbbfZZEWZs8bvPs2b4aJSzxbjFVjxQ",
+    authDomain: "vueconnect4.firebaseapp.com",
+    projectId: "vueconnect4",
+    storageBucket: "vueconnect4.appspot.com",
+    messagingSenderId: "1068821387814",
+    appId: "1:1068821387814:web:ce2ea81f6cfffcd444e4e7"
+  };
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
